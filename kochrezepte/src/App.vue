8d36a770fb1197d96c79a722fa60adaf8a53a21b
Valeriya Stange <valeriya.stange@gmail.com>
2025-06-28 17:11:16 +0200

    create Header component

--- kochrezepte/src/App.vue
+++ kochrezepte/src/App.vue
@@ -1,11 +1,28 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <!-- <nav><router-link to="/">Home</router-link> | <router-link to="/about">About</router-link></nav> -->
+  <Header></Header>
+  <router-view />
 </template>
 
-<style>
+<script>
+import Header from '@/components/Header.vue'
 
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+input {
+  border: 0;
+  border-radius: 8px;
+  width: 100%;
+  height: 30px;
+}
 </style>
---
+++ kochrezepte/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <input type="text" placeholder="Suche...">
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style scoped>
+header {
+    background-color: #f4bc39;
+    height: 150px;
+    padding: 16px;
+    display: flex;
+    align-items: flex-end;
+}
+</style>
