a6420dbe47c12a6790dc7a01e80e981fbd6174fd
Valeriya Stange <valeriya.stange@gmail.com>
2025-06-29 12:01:33 +0200

    create RecipeItem component with variables for content and styling

--- kochrezepte/src/components/RecipeItem.vue
+++ kochrezepte/src/components/RecipeItem.vue
@@ -1,12 +1,23 @@
 <template>
     <div class="recipeItemContainer">
-        <img src="@/assets/pizza.png" alt="">Mein Rezept
+        <img class="recipe-image" :src="myImagePizza" alt="">
+        <div class="right-side">
+            <div>{{ label }}</div>
+            <div class="bottom-row">{{ attributes }}</div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: 'RecipeItem',
+    data() {
+        return {
+            myImagePizza: require('@/assets/pizza.png'),
+            label: 'Pizza',
+            attributes: 'Italienische, Fettarm'
+        }
+    },
 }
 </script>
 
@@ -16,5 +27,30 @@ export default {
     padding: 8px;
     border-radius: 8px;
     margin: 8px;
+    display: flex;
+    gap: 26px;
+    justify-content: flex-start;
+    align-items: center;
+    text-align: center;
+    cursor: pointer;
+}
+
+.recipeItemContainer:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+}
+
+.recipe-image {
+    height: 100px;
+}
+
+.right-side {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
+
+.bottom-row {
+    color: rgba(0, 0, 0, 0.6);
+    font-size: 14px;
 }
 </style>
